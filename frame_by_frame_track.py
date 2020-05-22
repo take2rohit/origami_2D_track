beccc500c1ce52b2b701dc0c6b9f11d63c5455bb
Rohit Lal <rohitlal789@gmail.com>
2020-05-22 21:12:16 +0530

    updated maunual tracking code

--- frame_by_frame_track.py
+++ frame_by_frame_track.py
@@ -6,41 +6,38 @@ p3 = []
 
 global points
 points = []
+cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+
+
 def draw_circle(event,x,y,flags,param):
 	global mouseX,mouseY
 	if event == cv2.EVENT_LBUTTONDBLCLK:
-		cv2.circle(frame,(x,y),5,(255,0,0),-1)
+		cv2.circle(frame,(x,y),4,(255,0,0),-1)
 		mouseX,mouseY = x,y
 		points.append([x,y])
 		print(x,y)
-
-cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
-cap = cv2.VideoCapture('/home/rohit/projects/gamitrack/videos/tumble_crop.mp4')
+cap = cv2.VideoCapture('clipped_video/inchworm_forward_2.mp4')
 frame_no = 0
 
 while 1:
-	frame_no += 1
-	_,frame = cap.read()
-	frame = cv2.flip(frame, 0)
-	frame = cv2.flip(frame, 1)
-
-	if frame_no > 65:
-		# cv2.imshow('image',frame)
-			
+	try:
+		frame_no += 1
+		_,frame = cap.read()
 		key = cv2.waitKey(0) & 0xff
-		
-		if key  == ord("q"):
-			break
-		# if event == cv2.EVENT_LBUTTONDBLCLK:
-			
-		elif key == ord('a'):
-			print(mouseX,mouseY)
-	cv2.imshow('image',frame)
-	# print(points)
-	print(frame_no)
-	print(len(points)/2)
+		cv2.imshow('image',frame)
+		print("frame: ", frame_no)
+		print(len(points)/2)
 
-# np.save('origami_custom_track.npy', np.array(points))
-# np.save('magnet_origami_custom_track.npy', np.array(points))
-print('Tracking saved')
+		
+
+		if key  == ord("q"):
+			# np.save('saved_files/inchworm_forward/origami.npy', np.array(points))
+			# np.save('saved_files/inchworm_forward/magnet.npy', np.array(points))
+			break
+
+	except Exception as e:
+		# np.save('saved_files/inchworm_forward/origami.npy', np.array(points))
+		# np.save('saved_files/inchworm_forward/magnet.npy', np.array(points))
+		break
+print('Tracking saved')         
