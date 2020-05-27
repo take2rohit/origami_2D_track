a6c502aa32444090daa3ee352eb64f37c7e89ab0
Rohit Lal <rohitlal789@gmail.com>
2020-05-27 14:10:42 +0530

    added corrected csv

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -1,11 +1,10 @@
 import numpy as np
 import csv
 
-pts_origami = np.load('saved_files/inchworm_forward/magnet.npy')
+pts_magnet = np.load('saved_files/tumbling/magnet.npy')
 
-with open('saved_files/inchworm_forward/magnet.csv', 'w', newline='') as file:
+with open('saved_files/tumbling/magnet.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow( ['point 1','point 2','point 3'] )
-    for i in range(0, len(pts_origami), 3):
-        writer.writerow([pts_origami[i], pts_origami[i+1], pts_origami[i+2]])
-
+    writer.writerow( ['point 1','point 2'] )
+    for i in range(0, len(pts_magnet), 2):
+        writer.writerow([pts_magnet[i], pts_magnet[i+1]])
